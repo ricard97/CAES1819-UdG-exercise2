4529c6db060a76856c668ff4415afcf7c3e67958
ricard97 <35930115+ricard97@users.noreply.github.com>
2018-10-08 17:45:55 +0200

    Commit union implementat

--- src/main/java/edu/udg/caes/Exercise2.java
+++ src/main/java/edu/udg/caes/Exercise2.java
@@ -13,7 +13,19 @@ public class Exercise2 {
      */
     public static Vector union (Vector a, Vector b)
     {
-        throw new UnsupportedOperationException("Non implemented yet");
+        Vector res = new Vector();
+
+        for (int i = 0; i<a.size(); i++) {
+
+            res.add(a.get(i));
+        }
+
+        for (int i = 0; i<b.size(); i++) {
+
+            res.add(b.get(i));
+        }
+        return res;
+        //throw new UnsupportedOperationException("Non implemented yet");
     }
 
 }
