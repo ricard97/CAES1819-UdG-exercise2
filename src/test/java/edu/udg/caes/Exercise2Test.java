70a4644b897cbc656d4ef3fc6fd083a456f00244
ricard97 <35930115+ricard97@users.noreply.github.com>
2018-10-08 17:57:41 +0200

    Commit tests
    1 funciona amb codi
    la resta no funciona

--- src/test/java/edu/udg/caes/Exercise2Test.java
+++ src/test/java/edu/udg/caes/Exercise2Test.java
@@ -1,10 +1,97 @@
 package edu.udg.caes;
 
+import java.util.Vector;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Exercise2Test {
 
-    // Tests not implemented yet
+    @Test
+    public void be() {
 
+        Vector a = new Vector();
+        a.add(1);
+
+        Vector b = new Vector();
+        b.add(1);
+        b.add(2);
+
+        Vector res = new Vector();
+        res.add(1);
+        res.add(1);
+        res.add(2);
+
+        final Vector result = new Exercise2().union(a, b);
+        assertEquals(result, res);
+    }
+
+    @Test
+    public void firstNull() {
+        Vector a = null;
+        Vector b = new Vector();
+        b.add(1);
+
+        Vector res = new Vector();
+
+        final Vector result = new Exercise2().union(a, b);
+        assertEquals(result, null);
+    }
+
+    @Test
+    public void secondNull() {
+        Vector a = new Vector();
+        a.add(1);
+        Vector b = null;
+
+        Vector res = new Vector();
+
+        final Vector result = new Exercise2().union(a, b);
+        assertEquals(result, null);
+    }
+
+    @Test
+    public void no2SameNulls() {
+        Vector a = new Vector();
+        Vector b = new Vector();
+        Vector res = new Vector();
+
+        final Vector result = new Exercise2().union(a, b);
+        assertEquals(result, null);
+    }
+
+    @Test
+    public void noRepeats() {
+        Vector a = new Vector();
+        a.add(1);
+
+        Vector b = new Vector();
+        b.add(1);
+        b.add(2);
+
+        Vector res = new Vector();
+        res.add(1);
+        res.add(2);
+
+        final Vector result = new Exercise2().union(a, b);
+        assertEquals(result, res);
+    }
+
+    @Test
+    public void sameType() {
+        Vector a = new Vector();
+        a.add("r");
+        a.add("t");
+
+        Vector b = new Vector();
+        b.add(1);
+        b.add(2);
+
+        Vector res = new Vector();
+        res.add(1);
+        res.add(2);
+
+        final Vector result = new Exercise2().union(a, b);
+        assertEquals(result, null);
+    }
 }
